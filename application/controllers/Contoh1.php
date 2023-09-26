34bc78861cb2583ed14798445a168e025d09b01e
Rainidan25 <zaingaming120@gmail.com>
2023-09-26 20:19:28 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+    echo "<h1> Perkenalkan </h1>";
+    echo "Nama Saya Rainidan Zainnur Salam
+    Saya tinggal dimana aja boleh 
+    Olahraga yang saya sukai adalah Sepak Takraw";
+    }
+}
+?>
